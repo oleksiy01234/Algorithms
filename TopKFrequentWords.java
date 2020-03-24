61c076e5cf0793c28bc3a824e49b0cf25d224475
oleksiy01234 <45813923+oleksiy01234@users.noreply.github.com>
2020-03-23 20:49:12 -0500

    Create TopKFrequentWords.java

---
+++ TopKFrequentWords.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 692. Top K Frequent Words
+ * https://leetcode.com/problems/top-k-frequent-words/
+ * 
+ * Given a non-empty list of words, return the k most frequent elements.
+ * 
+ * Your answer should be sorted by frequency from highest to lowest. If two words have the same frequency,  * then the word with the lower alphabetical order comes first.
+ * 
+ * Example 1:
+ * Input: ["i", "love", "leetcode", "i", "love", "coding"], k = 2
+ * Output: ["i", "love"]
+ * Explanation: "i" and "love" are the two most frequent words.
+ *     Note that "i" comes before "love" due to a lower alphabetical order.
+ * 
+ * Example 2:
+ * Input: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"], k = 4
+ * Output: ["the", "is", "sunny", "day"]
+ * Explanation: "the", "is", "sunny" and "day" are the four most frequent words,
+ *     with the number of occurrence being 4, 3, 2 and 1 respectively.
+ * 
+ * Note:
+ * You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+ * Input words contain only lowercase letters.
+ * 
+ * Follow up:
+ * Try to solve it in O(n log k) time and O(n) extra space.
+ */
+public class TopKFrequentWords {
+
+  public List<String> topKFrequent(String[] words, int k) {
+    Map<String, Integer> count = new HashMap<>();
+    for (String word : words) {
+      count.put(word, count.getOrDefault(word, 0) + 1);
+    }
+
+    List<String> res = new ArrayList<>(count.keySet());
+    Collections.sort(res, (w1, w2) -> {
+      int diff = count.get(w2) - count.get(w1);
+      if (diff != 0) {
+        return diff;
+      }
+
+      return w1.compareTo(w2);
+    });
+
+    return res.subList(0, k);
+  }
+}
